e9fe89280430aa37ce32aa20b1ec86849dba5def
theMungai <mungaithesimon@gmail.com>
2025-05-06 18:59:34 +0300

    Python Functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum1 = add(1,2)
+print(sum1)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+result1 = halve(4)
+result2 = halve("four")
+
+print(result1)
+print(result2)
